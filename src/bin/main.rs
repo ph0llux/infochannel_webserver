f54387ab91904c435e5e77500059682362eb3987
ph0llux <ph0llux@pm.me>
2021-12-21 20:38:55 +0100

    configure different paths for release/debug mode

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -2,15 +2,24 @@
 #[macro_use] extern crate rocket;
 
 // - external
-use rocket::fs::{FileServer, relative};
+use rocket::fs::{FileServer};
+#[cfg(debug_assertions)]
+use rocket::fs::{relative};
 
 // - internal
 use lib::*;
 
 #[launch]
 fn rocket() -> _ {
+
+    #[cfg(debug_assertions)]
+    let fileserver_path = relative!("dashboard/build/web");
+    #[cfg(not(debug_assertions))]
+    let fileserver_path = "/var/www/dashboard";
+
+
     rocket::build()
-    .mount("/", FileServer::from(relative!("dashboard/build/web")))
+    .mount("/", FileServer::from(fileserver_path))
     .mount("/api", routes![datecontrol, add_newsfeed, get_all_newsfeeds])
     .attach(NewsDatabase::fairing())
 }
